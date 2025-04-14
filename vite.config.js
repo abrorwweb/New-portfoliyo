f78811ca30ac17ba97082ba70b9f0d268a8c5620
abrorjon0990 <abdurahimovabrorjon346@gmail.com>
2025-04-14 20:03:06 +0300

    finished-prekt

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import svgr from '@svgr/rollup';
+import svgr from '@svgr/rollup'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/',
   plugins: [react(), svgr()],
   server: {
     port: 3000,
